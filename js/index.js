e24f0a1847cddddc746fe06dc8b8ab98e8709ee8
snowball <snowbj1994@163.com>
2018-08-22 10:26:36 +0800

    修改页面

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ window.onload= function () {
         var panel=document.getElementsByClassName("panel");//页签，ie不兼容，换成id会成功
 
         var windowHeight=window.innerHeight;
-        scroll.style.height=windowHeight+"px";//获取窗口高度作为滑动距离
+        scroll.style.height=windowHeight+"px";//获取窗口高度作为滑动窗口高度
         for(var i=0;i<panel.length;i++){
             panel[i].style.height=windowHeight+"px";
         }
@@ -19,7 +19,7 @@ window.onload= function () {
                 delta=event.wheelDelta/120;
             }else if(event.detail){
                 delta=-event.detail/3;
-            }
+            }//五大浏览器（IE、Opera、 Safari、Firefox、Chrome）中Firefox 使用detail，其余四类使用wheelDelta
             if(delta){
                 handle(delta,inputC);
             }
@@ -27,10 +27,10 @@ window.onload= function () {
                 event.preventDefault();
             event.returnValue=false;
         };
-        if(window.addEventListener){
-            window.addEventListener('DOMMouseScroll',wheel,false);
+        if(window.addEventListener){//for ff
+            window.addEventListener('DOMMouseScroll',wheel,false);//将wheel函数监听到ff浏览器上
         }
-        window.onmousewheel=wheel;
+        window.onmousewheel=wheel;//触发鼠标滚轮事件
     };
     function handle(delta,arr) {
         var num;
@@ -42,7 +42,7 @@ window.onload= function () {
         if(delta>0 && num>0){//向上滚动
             num--;
             arr[num].checked=true;
-        }else if(delta<0 && num<4){//向下滚动
+        }else if(delta<0 && num<arr.length-1){//向下滚动
             num++;
             arr[num].checked=true;
         }
